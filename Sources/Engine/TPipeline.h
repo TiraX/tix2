f6cd1cc9b553ef228c2419bfeab5e15c80588026
TiraX <zhaoshuai.cn@gmail.com>
2018-07-23 23:30:51 +0800

    - Pipeline state. WIP.

--- Sources/Engine/TPipeline.h
+++ Sources/Engine/TPipeline.h
@@ -15,34 +15,36 @@ namespace tix
 		EPSO_DEPTH = 1 << 1,
 		EPSO_DEPTH_TEST = 1 << 2,
 		EPSO_STENCIL = 1 << 3,
-		EPSO_STENCIL_TEST = 1 << 4,
-
-		//EPSO_FILLMODE = 1 << 0,
-		//EPSO_CULL = 1 << 1,
-		//EPSO_DEPTH = 1 << 2,
-		//EPSO_DEPTH_TEST = 1 << 3,
-		////EPSO_STENCIL	= 1 << 4,
-		//EPSO_STENCIL_TEST = 1 << 5,
-		//EPSO_BLEND = 1 << 6,
-		//EPSO_FOG = 1 << 7,
-		//EPSO_SCISSOR = 1 << 8,
-		//EPSO_ALPHA_TEST = 1 << 9,
-		//EPSO_IGNORE_DEPTH_BUFFER = 1 << 10,	// for metal, some shaders do not need depth buffer
+		EPSO_STENCIL_TEST = 1 << 4
+	};
+	enum E_BLEND_FUNC
+	{
+		EBF_ZERO,
+		EBF_ONE,
+		EBF_SRC_COLOR,
+		EBF_ONE_MINUS_SRC_COLOR,
+		EBF_DEST_COLOR,
+		EBF_ONE_MINUS_DEST_COLOR,
+		EBF_SRC_ALPHA,
+		EBF_ONE_MINUS_SRC_ALPHA,
+		EBF_DST_ALPHA,
+		EBF_ONE_MINUS_DST_ALPHA,
+		EBF_CONSTANT_COLOR,
+		EBF_ONE_MINUS_CONSTANT_COLOR,
+		EBF_CONSTANT_ALPHA,
+		EBF_ONE_MINUS_CONSTANT_ALPHA,
+		EBF_SRC_ALPHA_SATURATE
 	};
 
-	//typedef struct D3D12_RENDER_TARGET_BLEND_DESC
-	//{
-	//	BOOL BlendEnable;
-	//	BOOL LogicOpEnable;
-	//	D3D12_BLEND SrcBlend;
-	//	D3D12_BLEND DestBlend;
-	//	D3D12_BLEND_OP BlendOp;
-	//	D3D12_BLEND SrcBlendAlpha;
-	//	D3D12_BLEND DestBlendAlpha;
-	//	D3D12_BLEND_OP BlendOpAlpha;
-	//	D3D12_LOGIC_OP LogicOp;
-	//	UINT8 RenderTargetWriteMask;
-	//} 	D3D12_RENDER_TARGET_BLEND_DESC;
+	enum E_BLEND_EQUATION
+	{
+		EBE_FUNC_ADD,
+		EBE_FUNC_SUBTRACT,
+		EBE_FUNC_REVERSE_SUBTRACT,
+		EBE_MIN,
+		EBE_MAX
+	};
+
 	struct TBlendState
 	{
 		uint8 SrcBlend;
@@ -53,22 +55,37 @@ namespace tix
 		uint8 BlendOpAlpha;
 		uint8 LogicOp;
 		uint8 RenderTargetWriteMask;
+
+		TBlendState()
+			: SrcBlend(EBF_SRC_ALPHA)
+			, DestBlend(EBF_ONE_MINUS_SRC_ALPHA)
+			, BlendOp(EBE_FUNC_ADD)
+			, SrcBlendAlpha(EBF_ONE)
+			, DestBlendAlpha(EBF_ZERO)
+			, BlendOpAlpha(EBE_FUNC_ADD)
+			, LogicOp(0)
+			, RenderTargetWriteMask(0)
+		{}
 	};
 
-	//typedef struct D3D12_RASTERIZER_DESC
-	//{
-	//	D3D12_FILL_MODE FillMode;
-	//	D3D12_CULL_MODE CullMode;
-	//	BOOL FrontCounterClockwise;
-	//	INT DepthBias;
-	//	FLOAT DepthBiasClamp;
-	//	FLOAT SlopeScaledDepthBias;
-	//	BOOL DepthClipEnable;
-	//	BOOL MultisampleEnable;
-	//	BOOL AntialiasedLineEnable;
-	//	UINT ForcedSampleCount;
-	//	D3D12_CONSERVATIVE_RASTERIZATION_MODE ConservativeRaster;
-	//} 	D3D12_RASTERIZER_DESC;
+	// Rasterizer definition
+	enum E_FILL_MODE
+	{
+		EFM_WIREFRAME = 0,
+		EFM_SOLID = 1,
+	};
+
+	enum E_FRONT_FACE
+	{
+		EFF_CW,
+		EFF_CCW
+	};
+
+	enum E_CULL_MODE
+	{
+		ECM_FRONT = 0,
+		ECM_BACK = 1
+	};
 	struct TRasterizerDesc
 	{
 		uint8 FillMode;
@@ -76,34 +93,53 @@ namespace tix
 		uint8 MultiSampleCount;
 		uint8 Reserved;
 		float DepthBias;
+
+		TRasterizerDesc()
+			: FillMode(EFM_SOLID)
+			, CullMode(ECM_BACK)
+			, MultiSampleCount(0)
+			, Reserved(0)
+			, DepthBias(0.f)
+		{}
 	};
 
-	//typedef struct D3D12_DEPTH_STENCILOP_DESC
-	//{
-	//	D3D12_STENCIL_OP StencilFailOp;
-	//	D3D12_STENCIL_OP StencilDepthFailOp;
-	//	D3D12_STENCIL_OP StencilPassOp;
-	//	D3D12_COMPARISON_FUNC StencilFunc;
-	//} 	D3D12_DEPTH_STENCILOP_DESC;
+	// Depth and Stencil definition
+	enum E_COMPARISON_FUNC
+	{
+		ECF_NEVER = 1,
+		ECF_LESS = 2,
+		ECF_LESS_EQUAL = 3,
+		ECF_EQUAL = 4,
+		ECF_GREATER = 5,
+		ECF_NOT_EQUAL = 6,
+		ECF_GREATER_EQUAL = 7,
+		ECF_ALWAYS = 8
+	};
 
-	//typedef struct D3D12_DEPTH_STENCIL_DESC
-	//{
-	//	BOOL DepthEnable;
-	//	D3D12_DEPTH_WRITE_MASK DepthWriteMask;
-	//	D3D12_COMPARISON_FUNC DepthFunc;
-	//	BOOL StencilEnable;
-	//	UINT8 StencilReadMask;
-	//	UINT8 StencilWriteMask;
-	//	D3D12_DEPTH_STENCILOP_DESC FrontFace;
-	//	D3D12_DEPTH_STENCILOP_DESC BackFace;
-	//} 	D3D12_DEPTH_STENCIL_DESC;
-
+	enum E_STENCIL_OP
+	{
+		ESO_KEEP = 1,
+		ESO_ZERO = 2,
+		ESO_REPLACE = 3,
+		ESO_INCR_SAT = 4,
+		ESO_DECR_SAT = 5,
+		ESO_INVERT = 6,
+		ESO_INCR = 7,
+		ESO_DECR = 8
+	};
 	struct TDepthStencilOpDesc
 	{
 		uint8 StencilFailOp;
 		uint8 StencilDepthFailOp;
 		uint8 StencilPassOp;
 		uint8 StencilFunc;
+
+		TDepthStencilOpDesc()
+			: StencilFailOp(ESO_KEEP)
+			, StencilDepthFailOp(ESO_KEEP)
+			, StencilPassOp(ESO_KEEP)
+			, StencilFunc(ECF_ALWAYS)
+		{}
 	};
 	struct TDepthStencilDesc
 	{
@@ -113,6 +149,13 @@ namespace tix
 		uint8 Reserved;
 		TDepthStencilOpDesc FrontFace;
 		TDepthStencilOpDesc BackFace;
+
+		TDepthStencilDesc()
+			: DepthFunc(ECF_LESS)
+			, StencilReadMask(0xff)
+			, StencilWriteMask(0xff)
+			, Reserved(0)
+		{}
 	};
 
 	//typedef struct D3D12_GRAPHICS_PIPELINE_STATE_DESC
@@ -148,6 +191,14 @@ namespace tix
 		TString GsName;
 		uint32 Flags;
 		TBlendState BlendState;
+		TRasterizerDesc RasterizerDesc;
+		TDepthStencilDesc DepthStencilDesc;
+		uint32 VsFormat;
+
+		TPipelineDesc()
+			: Flags(EPSO_DEPTH | EPSO_DEPTH_TEST)
+			, VsFormat(EVSSEG_POSITION)
+		{}
 	};
 
 	class FPipeline;
@@ -162,6 +213,7 @@ namespace tix
 		virtual void InitRenderThreadResource() override;
 		virtual void DestroyRenderThreadResource() override;
 
+		TPipelineDesc Desc;
 		FPipelinePtr PipelineResource;
 	protected:
 
