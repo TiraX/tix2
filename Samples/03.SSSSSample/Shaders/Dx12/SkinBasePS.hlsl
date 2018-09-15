8b9b213c836c34ccc40abf6f96c0448e8065e7f7
TiraX <zhaoshuai.cn@gmail.com>
2018-09-15 16:06:26 +0800

    - Add shader SkinBase.

---
+++ Samples/03.SSSSSample/Shaders/Dx12/SkinBasePS.hlsl
@@ -0,0 +1,37 @@
+#include "SkinBaseRS.hlsli"
+
+struct VSOutput
+{
+    sample float4 position : SV_Position;
+    sample float2 uv : TexCoord0;
+    sample float3 normal : Normal;
+    sample float3 tangent : Tangent;
+    sample float3 bitangent : Bitangent;
+};
+
+Texture2D<float3> texDiffuse		: register(t0);
+Texture2D<float3> texSpecular		: register(t1);
+Texture2D<float3> texNormal			: register(t2);
+
+
+cbuffer PSConstants : register(b0)
+{
+    float3 SunDirection;
+    float3 SunColor;
+    float3 AmbientColor;
+    float4 ShadowTexelSize;
+
+    float4 InvTileDim;
+    uint4 TileCount;
+    uint4 FirstLightIndex;
+}
+
+SamplerState sampler0 : register(s0);
+
+[RootSignature(SkinBase_RootSig)]
+float3 main(VSOutput vsOutput) : SV_Target0
+{
+    float3 diffuseAlbedo = texDiffuse.Sample(sampler0, vsOutput.uv);
+
+    return diffuseAlbedo;
+}
---
+++ Samples/03.SSSSSample/Shaders/Dx12/SkinBaseVS.hlsl
@@ -0,0 +1,38 @@
+#include "SkinBaseRS.hlsli"
+
+cbuffer VSConstants : register(b0)
+{
+    float4x4 vp;
+};
+
+struct VSInput
+{
+    float3 position : POSITION;
+	float3 normal : NORMAL;
+    float2 texcoord0 : TEXCOORD;
+    float3 tangent : TANGENT;
+};
+
+struct VSOutput
+{
+    float4 position : SV_Position;
+    float2 texCoord : TexCoord0;
+    float3 normal : Normal;
+    float3 tangent : Tangent;
+    float3 bitangent : Bitangent;
+};
+
+[RootSignature(SkinBase_RootSig)]
+VSOutput main(VSInput vsInput)
+{
+    VSOutput vsOutput;
+
+    vsOutput.position = mul(vp, float4(vsInput.position, 1.0));
+    vsOutput.texCoord = vsInput.texcoord0;
+
+    vsOutput.normal = vsInput.normal;
+    vsOutput.tangent = vsInput.tangent;
+	vsOutput.bitangent = cross(vsInput.normal, vsInput.tangent);;
+
+    return vsOutput;
+}
