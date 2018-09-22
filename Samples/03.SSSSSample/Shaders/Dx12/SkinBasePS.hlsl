5e00c6f7d622a9450461f0ee8a840ad039ead2f3
TiraX <zhaoshuai.cn@gmail.com>
2018-09-22 23:27:09 +0800

    - Add more texture to test.

--- Samples/03.SSSSSample/Shaders/Dx12/SkinBasePS.hlsl
+++ Samples/03.SSSSSample/Shaders/Dx12/SkinBasePS.hlsl
@@ -9,9 +9,10 @@ struct VSOutput
     sample float3 bitangent : Bitangent;
 };
 
-Texture2D<float3> texDiffuse		: register(t0);
-Texture2D<float3> texNormal			: register(t1);
-Texture2D<float3> texSpecular		: register(t2);
+Texture2D<float3> texDiffuse : register(t0);
+Texture2D<float3> texNormal : register(t1);
+Texture2D<float3> texSpecular : register(t2);
+Texture2D<float3> texBeckmann : register(t3);
 
 
 cbuffer PSConstants : register(b0)
@@ -31,12 +32,12 @@ SamplerState sampler0 : register(s0);
 [RootSignature(SkinBase_RootSig)]
 float4 main(VSOutput vsOutput) : SV_Target0
 {
-	float3 n = texNormal.Sample(sampler0, vsOutput.uv);
-	n = n * 2.0 - 1.0;
-	n.z = sqrt(1.0 - n.x * n.x - n.y * n.y);
+	float3 n = texBeckmann.Sample(sampler0, vsOutput.uv);
+	//n = n * 2.0 - 1.0;
+	//n.z = sqrt(1.0 - n.x * n.x - n.y * n.y);
 	//texDiffuse.Sample(sampler0, vsOutput.uv);
 
-	n = n * 0.5 + 0.5;
+	//n = n * 0.5 + 0.5;
 
     return float4(n, 1.0);
 }
