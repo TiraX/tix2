922000d603e00e4d182920edcf71b81ad3d8405a
TiraX-Mac <zhaoshuai.cn@gmail.com>
2018-12-20 00:16:05 +0800

    - Add iOS XCode workspace and project.

---
+++ Projects/iOS/TiX2/TiX2.h
@@ -0,0 +1,13 @@
+//
+//  TiX2.h
+//  TiX2
+//
+//  Created by Tirax on 2018/12/19.
+//  Copyright © 2018 zhaoshuai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TiX2 : NSObject
+
+@end
---
+++ Projects/iOS/TiX2/TiX2.m
@@ -0,0 +1,13 @@
+//
+//  TiX2.m
+//  TiX2
+//
+//  Created by Tirax on 2018/12/19.
+//  Copyright © 2018 zhaoshuai. All rights reserved.
+//
+
+#import "TiX2.h"
+
+@implementation TiX2
+
+@end
