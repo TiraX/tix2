cd78e49111ee2122fb6358404f914fc3f07c62bd
TiraX-Mac <zhaoshuai.cn@gmail.com>
2019-01-01 17:08:09 +0800

    - Add res converter mac project.

---
+++ Samples/02.MeshConverter/ResConverter/ResConverter/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ResConverter
+//
+//  Created by Tirax on 2019/1/1.
+//  Copyright © 2019 zhaoshuai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
