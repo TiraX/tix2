d2ff84deab404a507e4eb3a23838bc44b7cf3601
TiraX <zhaoshuai.cn@gmail.com>
2020-03-08 19:03:27 +0800

    - Clear project, start from begining.

--- Samples/06.GPUDriven0.2/GPUDrivenRenderer.h
+++ Samples/06.GPUDriven0.2/GPUDrivenRenderer.h
@@ -5,17 +5,7 @@
 
 #pragma once
 #include "SceneMetaInfos.h"
-#include "GPUComputeUniforms.h"
-#include "GPUTileFrustumCullCS.h"
-#include "GPUInstanceFrustumCullCS.h"
-#include "CopyVisibleTileCommandBuffer.h"
-#include "CopyVisibleInstances.h"
 #include "HiZDownSampleCS.h"
-#include "GPUInstanceOcclusionCullCS.h"
-#include "GPUClusterCullCS.h"
-#include "GenerateClusterCullIndirectCommand.h"
-#include "GenerateTriangleCullIndirectCommand.h"
-#include "GPUTriangleCullCS.h"
 
 class FGPUDrivenRenderer : public FDefaultRenderer
 {
@@ -29,15 +19,7 @@ public:
 	virtual void InitRenderFrame(FScene* Scene) override;
 	virtual void Render(FRHI* RHI, FScene* Scene) override;
 
-	void UpdateFrustumUniform(const SViewFrustum& Frustum);
-
 private:
-	void UpdateGPUCommandBuffer(FRHI* RHI, FScene * Scene);
-	void DrawGPUCommandBuffer(FRHI * RHI, FScene * Scene, FGPUCommandBufferPtr InGPUCommandBuffer);
-	void DrawGPUCommandBufferCluster(FRHI * RHI, FScene * Scene, FGPUCommandBufferPtr InGPUCommandBuffer);
-	void DrawSceneTiles(FRHI* RHI, FScene * Scene);
-
-	void SimluateCopyVisibleInstances(FRHI* RHI, FScene * Scene);
 
 private:
 	FSceneMetaInfos * SceneMetaInfo;
@@ -47,34 +29,13 @@ private:
 	FFullScreenRender FSRender;
 	FRenderTargetPtr RT_BasePass;
 
-	FRenderTargetPtr RT_DepthOnly;
-	FPipelinePtr DepthOnlyPipeline;
-
-	FTexturePtr HiZTexture;
-
 	FGPUCommandSignaturePtr GPUCommandSignature;
 	FGPUCommandBufferPtr GPUCommandBuffer;
-	FGPUCommandBufferPtr GPUCommandBufferTest;
 	FGPUCommandBufferPtr ProcessedGPUCommandBuffer;
-
-	FGPUCommandSignaturePtr GPUCommandSignatureCluster;
-	FGPUCommandBufferPtr GPUCommandBufferCluster;
 
 	FGPUCommandSignaturePtr PreZGPUCommandSignature;
 	FGPUCommandBufferPtr PreZGPUCommandBuffer;
 	FGPUCommandBufferPtr ProcessedPreZGPUCommandBuffer;
 
 	SViewFrustum Frustum;
-	FCameraFrustumUniformPtr FrustumUniform;
-
-	// Compute Tasks
-	FGPUInstanceFrustumCullCSPtr InstanceFrustumCullCS;
-	FCopyVisibleInstancesPtr CopyVisibleOccluders;
-	FCopyVisibleInstancesPtr CopyVisibleInstances;
-	FHiZDownSampleCSPtr HiZDownSample;
-	FGPUInstanceOcclusionCullCSPtr InstanceOcclusionCullCS;
-	//FGenerateClusterCullIndirectCommandPtr GenerateClusterCullCommand;
-	FGPUClusterCullCSPtr ClusterCullCS;
-	//FGenerateTriangleCullIndirectCommandPtr GenerateTriangleCullCommand;
-	FGPUTriangleCullCSPtr TriangleCullCS;
 };
