834bd7116c34031b265708c0b0790fbdf39a3733
Shuai Zhao <zhaoshuai2@kingsoft.com>
2020-07-13 20:39:36 +0800

    - Debuggin GTAO.

--- Samples/07.GTAO/Shaders/Dx12/S_GTAOCS.hlsl
+++ Samples/07.GTAO/Shaders/Dx12/S_GTAOCS.hlsl
@@ -18,8 +18,11 @@
                         "filter = FILTER_MIN_MAG_MIP_POINT)"
 
 static const float PI = 3.14159f;
-static const int MAX_STEPS = 35;
+static const int MAX_DIR = 16;
+static const float MAX_DIR_INV = 1.0 / float(MAX_DIR);
+static const int MAX_STEPS = 12;
 static const float STEP_LENGTH = 2.f;
+static const float ANGLE_STEP = PI * 2.f / MAX_DIR;
 static const int RAND_TEX_SIZE = 64 - 1;
 
 cbuffer FInfoUniform : register(b0)
@@ -27,7 +30,6 @@ cbuffer FInfoUniform : register(b0)
     float4 ScreenSize;  // xy = Size; zw = InvSize;
     float4 FocalLen;    // xy = FocalLen; zw = InvFocalLen;
     float4 Radius;      // x = radius; y = radius^2; z = 1.0/radius
-    float4 Falloff;     // x = scale, y = bias
 };
 
 Texture2D<float4> SceneNormal : register(t0);
@@ -72,109 +74,109 @@ float acosFast(float inX)
 
 float3 ScreenToViewPos(float2 UV, float Depth)
 {
-	UV = UV * float2(2.0, 2.0) - float2(1.0, 1.0);
-	return float3(UV * FocalLen.zw * Depth, Depth);
+    UV = UV * float2(2.0, 2.0) - float2(1.0, 1.0);
+    return float3(UV * FocalLen.zw * Depth, Depth);
 }
 
 float3 ScreenToViewPos(float2 UV)
 {
     float Depth = SceneDepth.SampleLevel(PointSampler, UV, 0);
-	return ScreenToViewPos(UV, Depth);
+    return ScreenToViewPos(UV, Depth);
 }
 
-float FalloffFn(float disSQ)
+float tangent(float3 T)
 {
-    return 2.0 * saturate(disSQ * Falloff.x + Falloff.y);
+    return -T.z / length(T.xy);
 }
 
-float SearchForLargestAngle(float2 inUV, float2 deltaUV, float3 ViewSpacePosition, float3 ViewDir)
+float tangent(float3 P, float3 S)
 {
-    float BiggestAngle = -1.0f;
-    //float PrevAngle = -1.0f;
+    return (P.z - S.z) / length(S.xy - P.xy);
+}
 
-    float2 UV = inUV;
+float3 MinDiff(float3 P, float3 R, float3 L)
+{
+    float3 V1 = R - P;
+    float3 V2 = P - L;
+    return (dot(V1, V1) < dot(V2, V2)) ? V1 : V2;
+}
 
-    [unroll]
-    for (int i = 1; i <= MAX_STEPS; i++)
-    {
-        UV += deltaUV;
+//----------------------------------------------------------------------------------
+float SearchForLargestAngle(float2 deltaUV,
+    float2 uv0,
+    float3 P,
+    float numSteps,
+    float randstep,
+    float3 dPdu,
+    float3 dPdv)
+{
+    // Randomize starting point within the first sample distance
+    //float2 uv = uv0 + snap_uv_offset(randstep * deltaUV);
+    float2 uv = uv0 + randstep * deltaUV;
 
-        float3 SampVS = ScreenToViewPos(UV);
-        float3 DiffV = (SampVS - ViewSpacePosition);
-        float distSQ = dot(DiffV, DiffV);
+    // Snap increments to pixels to avoid disparities between xy 
+    // and z sample locations and sample along a line
+    //deltaUV = snap_uv_offset(deltaUV);
 
-        [branch]
-            if (distSQ < Radius.y)
-            {
-                float ooDist = rsqrt(distSQ);
-                float cosh = ooDist * dot(DiffV, ViewDir);
+    // Compute tangent vector using the tangent plane
+    float3 T = deltaUV.x * dPdu + deltaUV.y * dPdv;
 
-                float falloff = 0.5;// FalloffFn(distSQ);
+    //float tanH = biased_tangent(T);
+    float tanH = tangent(T);
+    float sinH = tanH / sqrt(1.0f + tanH * tanH);
 
-                cosh = cosh - falloff;
-                BiggestAngle = max(BiggestAngle, cosh);//UpdateMaxAngle(inUV, BiggestAngle, cosh, PrevAngle);
-                //PrevAngle = cosh;
+    float angle = 0;
+    for (float j = 1; j <= numSteps; ++j) {
+        uv += deltaUV;
+        float3 S = ScreenToViewPos(uv);
+
+        // Ignore any samples outside the radius of influence
+        float3 D = S - P;
+        float d2 = dot(D, D);
+        if (d2 < Radius.y) {
+            float tanS = tangent(P, S);
+
+            [branch]
+            if (tanS > tanH) {
+                float cosS = rsqrt(1.f + tanS * tanS);
+                angle = acosFast(cosS);
+                //// Accumulate AO between the horizon and the sample
+                //float sinS = tanS / sqrt(1.0f + tanS * tanS);
+                //float r = sqrt(d2) * Radius.z;
+                //// ao += falloff(r) * (sinS - sinH);
+                //ao += (sinS - sinH);
+
+                // Update the current horizon angle
+                tanH = tanS;
             }
-
+        }
     }
 
-
-    return BiggestAngle;
+    return angle;
 }
 
-
-// Given a screen space Axis this will search for the min and max angles
-float2 SearchAxisForAngles(float2 UV, float3 ScreenDir, float3 ViewDir, float3 ViewSpacePos, float2 DeltaUV, float Offset)
+float2 SearchAxisForAngles(float2 deltaUV,
+    float2 uv0,
+    float3 P,
+    float numSteps,
+    float randstep,
+    float3 dPdu,
+    float3 dPdv)
 {
-    //float2 DeltaUV = Scale * float2(GTAOParams[2].z * ScreenDir.x, GTAOParams[2].w * ScreenDir.y);
-    //DeltaUV.y *= -1;
+    float h1 = SearchForLargestAngle(deltaUV, uv0, P, numSteps, randstep, dPdu, dPdv);
+    float h2 = -SearchForLargestAngle(-deltaUV, uv0, P, numSteps, randstep, dPdu, dPdv);
 
-    float Ang1 = SearchForLargestAngle(UV + (DeltaUV * Offset), DeltaUV, ViewSpacePos, ViewDir);
-    float Ang2 = SearchForLargestAngle(UV - (DeltaUV * Offset), -DeltaUV, ViewSpacePos, ViewDir);
-    return float2(Ang1, Ang2);
+    return float2(h1, h2);
 }
 
-float ComputeInnerIntegral(float2 Angles, float3 ScreenDir, float3 ViewDir, float3 ViewSpaceNormal, float SceneDepth)
+float ComputeInnerIntegral(float2 Angles, float NoV, float n)
 {
+    float cosN = NoV;
+    float sinN = sqrt(1.f - NoV * NoV);
+    float a1 = -cos(Angles.x * 2.f - n);
+    float a2 = -cos(Angles.y * 2.f - n);
 
-    float3 PlaneNormal = normalize(cross(ScreenDir, ViewDir));
-    float3 Perp = cross(ViewDir, PlaneNormal);
-    float3 ProjNormal = ViewSpaceNormal - PlaneNormal * dot(ViewSpaceNormal, PlaneNormal);
-
-    float LenProjNormal = length(ProjNormal);
-    float RecipMag = 1.0f / (LenProjNormal + 1e-6);
-    float CosAng = dot(ProjNormal, Perp) * RecipMag;
-    float Gamma = acosFast(CosAng) - (PI * 0.5);
-    float CosGamma = dot(ProjNormal, ViewDir) * RecipMag;
-    float SinGamma = CosAng * -2.0f;
-
-
-    Angles.x = Gamma + max(-Angles.x - Gamma, -((PI * 0.5)));
-    Angles.y = Gamma + min(Angles.y - Gamma, ((PI * 0.5)));
-
-
-    LenProjNormal = 1;
-
-
-
-
-    float AO = (LenProjNormal * 0.25 *
-        ((Angles.x * SinGamma + CosGamma - cos((2.0 * Angles.x) - Gamma)) +
-            (Angles.y * SinGamma + CosGamma - cos((2.0 * Angles.y) - Gamma))));
-    AO = saturate(AO / (PI * 0.5));
-
-
-
-
-
-
-
-
-    //float Mul = ScreenSpaceAOParams[4].x;
-    //float Add = ScreenSpaceAOParams[4].y;
-    //AO = lerp(AO, 1, saturate(SceneDepth * Mul + Add));
-
-    return AO;
+    return (a1 + cosN + Angles.x * 2.f * sinN + a2 + cosN + Angles.y * 2.f * sinN) * 0.25f;
 }
 
 [RootSignature(GTAO_RootSig)]
@@ -183,12 +185,10 @@ void main(uint3 groupId : SV_GroupID, uint3 threadIDInGroup : SV_GroupThreadID, 
 {
     if (dispatchThreadId.x >= (uint)ScreenSize.x || dispatchThreadId.y >= (uint)ScreenSize.y)
         return;
-    
+
     float2 PixelCenter = dispatchThreadId.xy + float2(0.5, 0.5);
     float2 UV = PixelCenter * ScreenSize.zw;
-	float3 P = ScreenToViewPos(UV);
-
-    float3 ViewDir = -normalize(P);
+    float3 P = ScreenToViewPos(UV);
 
     // Project radius from eye space to texture space
     float2 StepSize = 0.5f * Radius.x * FocalLen.xy / P.z;
@@ -199,26 +199,39 @@ void main(uint3 groupId : SV_GroupID, uint3 threadIDInGroup : SV_GroupThreadID, 
 
     float3 L, R, T, B;
     L = ScreenToViewPos(UV + float2(-ScreenSize.z, 0));
-    R = ScreenToViewPos(UV + float2( ScreenSize.z, 0));
-    T = ScreenToViewPos(UV + float2(0,  ScreenSize.w));
+    R = ScreenToViewPos(UV + float2(ScreenSize.z, 0));
+    T = ScreenToViewPos(UV + float2(0, ScreenSize.w));
     B = ScreenToViewPos(UV + float2(0, -ScreenSize.w));
     float3 N = normalize(cross(R - L, T - B));
+    float4 TangentPlane = float4(N, dot(P, N));
+
+    float3 dPdU = MinDiff(P, R, L);
+    float3 dPdV = MinDiff(P, T, B) * (ScreenSize.y * ScreenSize.z);
 
     // load random (cos(alpha), sin(alpha), jitter)
     float3 RandDir = RandomTex.Load(int3((int)(dispatchThreadId.x & RAND_TEX_SIZE), (int)(dispatchThreadId.y & RAND_TEX_SIZE), 0)).xyz;
     RandDir = RandDir * 2.f - 1.f;
 
+    float NoV = dot(N, float3(0, 0, 1));
+    float n = acosFast(NoV);
+
     float AO = 0.f;
+    [unroll]
+    for (int d = 0; d < MAX_DIR; d++)
+    {
+        float2 Dir = float2(cos(d * ANGLE_STEP), sin(d * ANGLE_STEP));
+        float2 DeltaUV = float2(Dir.x * RandDir.x - Dir.y * RandDir.y, Dir.x * RandDir.y + Dir.y * RandDir.x) * StepSize.xy;
 
-    float3 ScreenDir = float3(1, 0, 0);// float3(RandDir.x, RandDir.y, 0.0);
-    float2 DeltaUV = float2(ScreenDir.x * RandDir.x - ScreenDir.y * RandDir.y, ScreenDir.x * RandDir.y + ScreenDir.y * RandDir.x) * StepSize.xy;
-    float2 Angles = SearchAxisForAngles(UV, ScreenDir, ViewDir, P, DeltaUV, 0.0);// RandDir.z);
+        float2 Angles = SearchAxisForAngles(DeltaUV, UV, P, NumSteps, RandDir.z, dPdU, dPdV);
 
-    Angles.x = acosFast(Angles.x);
-    Angles.y = acosFast(Angles.y);
+        //Angles.x = acosFast(Angles.x);
+        //Angles.y = acosFast(Angles.y);
 
-    AO += ComputeInnerIntegral(Angles, ScreenDir, ViewDir, N, P.z);
+        AO += ComputeInnerIntegral(Angles, NoV, n);
+    }
+
+    AO = 1.f - AO / MAX_DIR;
 
 
-	AOResult[dispatchThreadId.xy] = AO;
+    AOResult[dispatchThreadId.xy] = AO;
 }
